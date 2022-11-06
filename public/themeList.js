280898bba9dda835385637ccf02201abf674b6cc
Lucifer25x <74368520+Lucifer25x@users.noreply.github.com>
2022-11-06 13:41:22 +0400

    Update themeList.js

--- public/themeList.js
+++ public/themeList.js
@@ -1,11 +1,13 @@
-const fs = require('fs');
-const path = require('path');
-const list = document.getElementById('list');
+const fs = require("fs");
+const path = require("path");
+const list = document.getElementById("list");
 
-let themeList = fs.readdirSync(path.join(path.dirname(__dirname), 'node_modules', 'brace', 'theme'));
+let themeList = fs.readdirSync(
+  path.join(path.dirname(__dirname), "node_modules", "brace", "theme")
+);
 
-for(let i = 0; i < themeList.length; i++){
-    let li = document.createElement('li');
-    li.innerText = themeList[i].slice(0, themeList[i].indexOf('.'));
-    list.appendChild(li);
+for (let i = 0; i < themeList.length; i++) {
+  let li = document.createElement("li");
+  li.innerText = themeList[i].slice(0, themeList[i].indexOf("."));
+  list.appendChild(li);
 }
